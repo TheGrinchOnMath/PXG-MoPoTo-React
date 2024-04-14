6636c3f30a511ec8dc2f7e14d5ef1ce71468c670
TheGrinchOnMath <jonathanwakeman@hotmail.com>
2024-04-14 16:07:27 +0200

    sync to access the files from another device

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import Button from "./shared/button.js";
 
 
 function App() {
+  const props = {id:"id", func:{name:"stuff", innertext:"I am a button!",info:{info1:null, info2:null}, args:{arg1:null, arg2:null}}};
   return (
     <div className="App">
       <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.<br/>
+          <Button props={props} />
           {/*Here's a <Button id="test!" function ="stuff" info="{JSON: 'JSON'}" innertext="text"/>*/}
         </p>
       </header>
     </div>
   )
 }
+
 
 export default App;
--- src/shared/button.js
+++ src/shared/button.js
@@ -1,13 +1,20 @@
-function Button(props) {
+function Button(props) {/*
 	const ID = this.props.id;
-	const func = this.props.function; // may need to be tweaked, this contains the function to be added as event handler under string form
+	const args = this.props.func.arguments;
+	const func = this.props.func.name; // may need to be tweaked, this contains the function to be added as event handler under string form
 	const text = this.props.innertext; // string containing the text that is to be displayed into the button
 	const INFO = this.props.info; // this is a JSON string that stores information such as side and faction
 	return (
-		<button id={ID} onClick={func} info={INFO}>
+		<button id={ID} onClick={() => {window[func](args)}} info={INFO}>
 			{text}
 		</button>
-	);
+	);*/
+	return (
+		<button>Click me!, [props]</button>
+	)
 }
+
+
+const stuff = () => alert("button pressed!");
 
 export default Button;
